f42a2775ee212f40877126d9cf6cb01426dda6ff
joaohenriquepda <joaohenrique.p.almeida@gmail.com>
2017-05-04 10:13:28 -0300

    Refactoring functions

---
+++ TypeStruct.h
@@ -0,0 +1,9 @@
+#ifndef TypeStruct
+#define TypeStruct
+
+
+struct info_thread{
+  int number;
+  char type_thread[10];
+};
+#endif /* TypeStruct_h */
--- main.c
+++ main.c
@@ -5,66 +5,71 @@
 #include <pthread.h>
 #include <signal.h>
 #include <unistd.h>
+#include "TypeStruct.h"
 
 int TERMINATE_FLAG = 0;
+char NAME_FILE[20];
+int NUMBER_THREADS=4;
 
 void set_end_flag(int sig){
   TERMINATE_FLAG = 1;
 }
 
-struct info_thread_t{
-  int number;
-  int qtd_lines;
-};
+void *save_log(void *args){
+  struct info_thread *info = (struct info_thread *) args;
 
-void *print(void *args){
-  struct info_thread_t *info = (struct info_thread_t *) args;
+
   while(!TERMINATE_FLAG){
     // Escrita de arquivo
-    printf("%d", info->number);
+    printf("%d\n", info->number);
     usleep(150);
   }
 }
 
+void file_insert(){
+
+  strcpy(NAME_FILE,strcat(NAME_FILE,".txt"));
+  FILE* file;
+
+  file = fopen(NAME_FILE,"wb");
+  if (file == NULL) {
+    printf("Error ao abrir arquivo");
+  }
+  fclose(file);
+}
+
+int terminate_program(){}
 
 int main(int argc, char const *argv[]) {
 
-  char *name_file;
 
-  name_file = argv[1];
-  name_file =  strcat(name_file,".txt");
-  FILE* file;
-
-  file = fopen(name_file,"wb");
-  if (file == NULL) {
-    printf("Error ao abrir arquivo");
-    exit(1);
-  }
-  printf("%s\n",name_file);
-
-  pthread_t thread[4];
-  struct info_thread_t info_thread[4];
-
-  //Cria 4 threads
   int i;
-  for (i = 0; i < 4; i++){
-    //printf("%d\n",i);
-    pthread_create(&thread[i],NULL,&print,&info_thread[i]);
+  strcpy(NAME_FILE,argv[1]);
+  //verificar onde é o melho lugar
+  file_insert();
+
+  pthread_t thread[NUMBER_THREADS];
+
+
+  struct info_thread info_thread[NUMBER_THREADS];
+
+  //Cria as threads
+  for (i = 0; i < NUMBER_THREADS; i++){
+      pthread_create(&thread[i],NULL,&save_log,&info_thread[i]);
   }
 
   while(!TERMINATE_FLAG){
     signal(SIGINT, set_end_flag);
   }
 
-if(TERMINATE_FLAG){
-  printf("\n[aviso]: Termino Solicitado. Aguardando threads...\n");
-  for (i = 0; i < 4; i++){
-     pthread_join(thread[i], NULL);
+  if(TERMINATE_FLAG){
+    printf("\n[aviso]: Termino Solicitado. Aguardando threads...\n");
+    for (i = 0; i < NUMBER_THREADS; i++){
+       pthread_join(thread[i], NULL);
+    }
+    printf("[aviso]: Aplicacao encerrada\n");
+    return 0;
   }
-  printf("[aviso]: Aplicacao encerrada\n");
-  return 0;
-}
 
- fclose(file);
   return 0;
 }
