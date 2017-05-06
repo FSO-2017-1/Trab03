2f6111257651bd0d1e28fc887b696756c364c240
Karine Valença <valenca.karine@gmail.com>
2017-05-06 18:48:51 -0300

    Criada thread produtora que gera numeros aleatorios e guarda num buffer

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #define _DEFAULT_SOURCE
+#define BUFFER_SIZE 50
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
 #include "TypeStruct.h"
 
 int TERMINATE_FLAG = 0;
 char NAME_FILE[20];
-int NUMBER_THREADS=4;
+int NUMBER_THREADS=4; 
 
 void set_end_flag(int sig){
   TERMINATE_FLAG = 1;
@@ -40,8 +42,23 @@ void file_insert(){
 
 int terminate_program(){}
 
-int main(int argc, char const *argv[]) {
+int producer_thread(void *args){
+  struct info_thread *info = (struct info_thread *) args;
+  int producer_buffer[BUFFER_SIZE];
+  int i = 0;
+  for(i=0; i<BUFFER_SIZE; i++){
+    while(!TERMINATE_FLAG){   
+      int producer_random_number = rand();
+      producer_buffer[i] = producer_random_number;
+      printf("Thread produtora criada");
+      printf(" i= %d random number = %d\n", i, producer_random_number);
+      sleep(5);
+    }
+  }
+}
 
+int main(int argc, char const *argv[]) {
+  srand(time(NULL));
 
   int i;
   strcpy(NAME_FILE,argv[1]);
@@ -53,10 +70,13 @@ int main(int argc, char const *argv[]) {
 
   struct info_thread info_thread[NUMBER_THREADS];
 
+  //Cria a thread produtora
+  pthread_create(&thread[0], NULL, &producer_thread, &info_thread[0]);
+
   //Cria as threads
-  for (i = 0; i < NUMBER_THREADS; i++){
+  /*for (i = 0; i < NUMBER_THREADS; i++){
       pthread_create(&thread[i],NULL,&save_log,&info_thread[i]);
-  }
+  }*/
 
   while(!TERMINATE_FLAG){
     signal(SIGINT, set_end_flag);
